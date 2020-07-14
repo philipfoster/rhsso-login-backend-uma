9482f78bb37456d76f189a240fd785822f9c9b33
Philip <pfoster@redhat.com>
2020-07-14 17:00:13 -0500

    Remove SAML, setup keycloak authentication

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
+// https://medium.com/@ramandeep.singh.1983/enterprise-web-app-authentication-using-keycloak-and-node-js-c10b0e26b80d
+
 const express = require('express')
-require('express-router');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const SamlStrategy = require('passport-saml').Strategy;
-const passport = require('passport')
+const _keycloak = require('keycloak-connect')
+const session = require('express-session')
 
 const port = 3001
 const app = express()
 
+
+// FOR TESTING ONLY. This disables TLS cert validation on requests to the SSO server.
+// Do not deploy with this enabled.
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+const memoryStore = new session.MemoryStore();
+
+
+const keycloak = new _keycloak({
+  store: new session.MemoryStore()
+}, "./keycloak.json");
+
+// let kcConfig = {
+//   clientId: 'react-test-app',
+//   bearerOnly: false,
+//   serverUrl: 'https://ec2-3-130-236-137.us-east-2.compute.amazonaws.com:8443/auth/',
+//   realm: 'test-app',
+// };
+
+app.use(session({
+  store: memoryStore,
+  secret: 'secret'
+}));
+
+
+app.use(keycloak.middleware())
+
+
 const helloRoute = require('./routes/hello')
-const authCallbackRoute = require('./routes/authCallback')
-const loginRoute = require('./routes/login')
-
-passport.use(new SamlStrategy(
-    {
-      path: '/login/callback',
-      entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php', // TODO: Find RHSSO entry point
-      issuer: 'passport-saml'
-    },
-    function(profile, done) {
-      console.log(`profile = ${profile}  \n done = ${done}`)
-    })
-);
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', helloRoute)
-app.use('/login/callback', authCallbackRoute)
-app.use('/login', loginRoute)
-
+// console.log("type = " + typeof keycloak.protect)
+app.use('/',
+    keycloak.protect((token) => {
+      console.log(token)
+      return token.hasRealmRole("test-role")
+    }),
+    helloRoute)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
